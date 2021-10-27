462345798d74aa2be90210c4b5058f061dc57ae0
harryrosadi <harry.harry.rosadi@gmail.com>
2021-10-27 23:04:53 +0700

    user done, token not yet

--- src/main/java/com/bukaParkir/model/Role.java
+++ src/main/java/com/bukaParkir/model/Role.java
@@ -2,13 +2,17 @@ package com.bukaParkir.model;
 
 import com.bukaParkir.common.auditable.ModelBase;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "role")
 public class Role extends ModelBase {
 
+    @NotNull
+    @Column(name = "name")
     private String name;
 
     public Role(String name) {
--- src/main/java/com/bukaParkir/repository/UserRepository.java
+++ src/main/java/com/bukaParkir/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.bukaParkir.repository;
 
-public interface UserRepository {
+import com.bukaParkir.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
 }
